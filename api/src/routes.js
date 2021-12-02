cb6346a40003838c42474282ea635dc9c6b9515d
Hermes-Francisco <franciscohermes2000@hotmail.com>
2021-12-01 21:26:38 -0300

    basic structure

---
+++ api/src/routes.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const HelloController = require('./controllers/HelloController');
+const routes = new Router();
+
+routes.get('/', HelloController.hello);
+
+module.exports = routes;
