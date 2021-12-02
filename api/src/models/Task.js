08b923158b5c1dd10808397d4894b71b132a5398
Hermes Francisco <55466449+Hermes-Francisco@users.noreply.github.com>
2021-12-02 20:54:36 -0300

    Adicionei a vírgula
    Co-authored-by: DharneyCas <58607328+DharneyCas@users.noreply.github.com>

--- api/src/models/Task.js
+++ api/src/models/Task.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
   title: String,
-  body: String
+  body: String,
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
