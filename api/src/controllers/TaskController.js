0f1ee918ac87cc8ae71cd774b31748eabd0c3dc4
Nathanno11 <nathanno11@users.noreply.github.com>
2021-12-03 20:40:27 -0300

    Foi criada a rota

--- api/src/controllers/TaskController.js
+++ api/src/controllers/TaskController.js
@@ -7,6 +7,12 @@ class TaskController {
     const resposta = await Task.create({ title, body });
     return res.status(201).json(resposta);
   }
+
+  async index (req, res) {
+    const resposta = await Task.find();
+
+    return res.status(200).json(resposta);
+  }
 }
 
 module.exports = new TaskController();
