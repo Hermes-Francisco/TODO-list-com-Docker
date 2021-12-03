80cf5c9e7f054d1c2a0f50bb3eef0b3502717187
Hermes-Francisco <franciscohermes2000@hotmail.com>
2021-12-02 22:36:08 -0300

    lint

--- api/src/controllers/TaskController.js
+++ api/src/controllers/TaskController.js
@@ -1,13 +1,12 @@
-const Task = require("../models/Task");
+const Task = require('../models/Task');
 
 class TaskController {
-    async create(req, res) {
-        const { title, body } = req.body;
+  async create(req, res) {
+    const { title, body } = req.body;
 
-        let resposta = await Task.create({title, body});
-
-        return res.status(201).json(resposta);
-    }
+    const resposta = await Task.create({ title, body });
+    return res.status(201).json(resposta);
+  }
 }
 
 module.exports = new TaskController();
