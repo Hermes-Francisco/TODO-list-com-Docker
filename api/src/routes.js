449edf592f9ac69043cb7b0fae77b49efe842113
Hermes-Francisco <franciscohermes2000@hotmail.com>
2021-12-04 13:42:17 -0300

    route

--- api/src/routes.js
+++ api/src/routes.js
@@ -5,8 +5,9 @@ const TaskController = require('./controllers/TaskController');
 const routes = new Router();
 
 routes.get('/', HelloController.hello);
-routes.get('/task', TaskController.index);
 routes.post('/task', TaskController.create);
+routes.get('/task', TaskController.index);
+routes.get('/task/:task', TaskController.show);
 routes.put('/task/:task', TaskController.update);
 
 module.exports = routes;
